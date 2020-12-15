067777c4cab1b8c297e8fbdd76d8ca1cb3ec55ff
vinay kumar <kammari.chary@mphasis.com>
2020-12-15 11:49:04 +0530

    First commit

---
+++ NetStatus.h
@@ -0,0 +1,18 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by ann paul on 15/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
---
+++ Sources/NetStatus.swift
@@ -0,0 +1,92 @@
+//
+//  NetStatus.swift
+//  NetStatusDemo
+//
+//  Created by Gabriel Theodoropoulos.
+//  Copyright © 2019 Appcoda. All rights reserved.
+//
+
+import Foundation
+import Network
+
+ public class NetStatus {
+    
+    // MARK: - Properties
+    
+    static  public let shared = NetStatus()
+    
+    public var monitor: NWPathMonitor?
+    
+    public var isMonitoring = false
+    
+    public var didStartMonitoringHandler: (() -> Void)?
+    
+    public var didStopMonitoringHandler: (() -> Void)?
+    
+    public var netStatusChangeHandler: (() -> Void)?
+    
+    
+    public var isConnected: Bool {
+        guard let monitor = monitor else { return false }
+        return monitor.currentPath.status == .satisfied
+    }
+    
+    
+    public  var interfaceType: NWInterface.InterfaceType? {
+        guard let monitor = monitor else { return nil }
+        
+        return monitor.currentPath.availableInterfaces.filter {
+            monitor.currentPath.usesInterfaceType($0.type) }.first?.type
+    }
+    
+    
+    public  var availableInterfacesTypes: [NWInterface.InterfaceType]? {
+        guard let monitor = monitor else { return nil }
+        return monitor.currentPath.availableInterfaces.map { $0.type }
+    }
+    
+    
+    public var isExpensive: Bool {
+        return monitor?.currentPath.isExpensive ?? false
+    }
+    
+    
+    // MARK: - Init & Deinit
+    
+    private init() {
+        
+    }
+    
+    
+    deinit {
+        stopMonitoring()
+    }
+    
+    
+    // MARK: - Method Implementation
+    
+    public func startMonitoring() {
+        guard !isMonitoring else { return }
+        
+        monitor = NWPathMonitor()
+        let queue = DispatchQueue(label: "NetStatus_Monitor")
+        monitor?.start(queue: queue)
+        
+        monitor?.pathUpdateHandler = { _ in
+            self.netStatusChangeHandler?()
+        }
+        
+        isMonitoring = true
+        didStartMonitoringHandler?()
+    }
+    
+    
+    public func stopMonitoring() {
+        guard isMonitoring, let monitor = monitor else { return }
+        monitor.cancel()
+        self.monitor = nil
+        isMonitoring = false
+        didStopMonitoringHandler?()
+    }
+    
+}
